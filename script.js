401669109078ff9c25b3c41b3ad10bb339d54afa
soko <sajidaaitsimhand1@gmail.com>
2025-08-04 04:35:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+const form = document.getElementById('registration-form');
+const feedbackDiv = document.getElementById('form-feedback');
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+	
+
+		const username = document.getElementById('username').value.trim();  // ✅ Fixed: get elements properly
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+        
+		let isValid = true;
+		let message = [];
+
+		if (username.length < 3)
+		{
+			isValid = false;
+			message.push('The username has to be more than 3 characters');
+		}
+		if (!email.includes('@') || !email.includes('.'))
+		{
+			isValid = false;
+			message.push('Please enter the right email');
+		}
+		if (password.length < 8)
+		{
+			isValid = false;
+			message.push('You need a strong passwprd')
+		}
+		feedbackDiv.style.display = "block";
+		if (isValid)
+		{
+			feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = "#28a745";
+		}
+		else{
+			feedbackDiv.innerHTML = messages.join('<br>');
+            feedbackDiv.style.color = "#dc3545";
+		}
+	});
+});
